7f86f9aebfd7e4780b26e81f48fbf047d3c8cb5f
Josh Cheek <josh.cheek@gmail.com>
2012-03-10 11:34:48 -0600

    Add RSpec failure messages

--- lib/mockingbird/rspec_matchers.rb
+++ lib/mockingbird/rspec_matchers.rb
@@ -1,26 +1,65 @@
+handler = Struct.new :verb, :instance do
+  def invocations
+    instance.invocations(verb)
+  end
+
+  def times_invoked
+    invocations.size
+  end
+
+  def match?
+    times_invoked > 0
+  end
+
+  def failure_message_for_should
+    "was never told to #{verb}"
+  end
+
+  def failure_message_for_should_not
+    time_or_times = times_invoked > 1 ? 'times' : 'time'
+    "shouldn't have been told to #{verb}, but was told to #{verb} #{times_invoked} #{time_or_times}"
+  end
+end
+
+
+match_num_times = Module.new do
+  attr_accessor :expected_times_invoked
+  def match?
+    expected_times_invoked == times_invoked
+  end
+
+  def failure_message_for_should
+    "should have been told to wink #{expected_times_invoked} " \
+    "time#{'s' if expected_times_invoked != 1} but was told to wink " \
+    "#{times_invoked} time#{'s' if times_invoked != 1}"
+  end
+
+  def failure_message_for_should_not
+    "shouldn't have been told to wink #{expected_times_invoked} " \
+      "time#{'s' if expected_times_invoked != 1}, but was"
+  end
+end
+
+
 RSpec::Matchers.define :have_been_told_to do |verb|
-  invocations = nil
+  use_case = handler.new verb
+
   match do |mocked_instance|
-    invocations = mocked_instance.invocations(verb)
-    if @n_times
-      invocations.size == @n_times
-    else
-      !invocations.empty?
-    end
+    use_case.instance = mocked_instance
+    use_case.match?
   end
 
   chain :times do |number|
-    @n_times = number
+    use_case.extend match_num_times
+    use_case.expected_times_invoked = number
   end
 
-  failure_message_for_should do |actual|
-    invocations.inspect
-    # "#{actual.invocation(verb).inspect} should have included #{arg.inspect} "
+  failure_message_for_should do
+    use_case.failure_message_for_should
   end
 
   failure_message_for_should_not do |actual|
-    invocations.inspect
-    # "should not have been #{arg.inspect}, but was #{actual.inspect}"
+    use_case.failure_message_for_should_not
   end
 
   description do
--- spec/rspec_matchers_spec.rb
+++ spec/rspec_matchers_spec.rb
@@ -5,13 +5,34 @@ describe 'RSpec matchers' do
   let(:mocked_class) { Mockingbird.song_for Class.new }
 
   describe 'have_been_told_to' do
-    example 'default example' do
-      mocked_class.sing :kick, default: []
-      mock = mocked_class.new
-      mock.should_not have_been_told_to :kick
-      mock.kick
-      mock.should have_been_told_to :kick
+    describe 'default use case' do
+      before { mocked_class.sing :kick, default: [] }
+      let(:instance) { mocked_class.new }
+
+      example 'passes if has been invoked at least once' do
+        instance.should_not have_been_told_to :kick
+        instance.kick
+        instance.should have_been_told_to :kick
+        instance.kick
+        instance.should have_been_told_to :kick
+      end
+
+      example 'failure message for should' do
+        expect { instance.should have_been_told_to :kick }.to \
+          raise_error(RSpec::Expectations::ExpectationNotMetError, /was never told to kick/)
+      end
+
+      example 'failure message for should not' do
+        instance.kick
+        expect { instance.should_not have_been_told_to :kick }.to \
+          raise_error(RSpec::Expectations::ExpectationNotMetError, /shouldn't have been told to kick, but was told to kick 1 time/)
+
+        instance.kick
+        expect { instance.should_not have_been_told_to :kick }.to \
+          raise_error(RSpec::Expectations::ExpectationNotMetError, /shouldn't have been told to kick, but was told to kick 2 times/)
+      end
     end
+
 
     example 'with arguments' do
       pending
@@ -21,14 +42,36 @@ describe 'RSpec matchers' do
       mock.should have_been_told_to(:kick).with(1, 2, 3)
     end
 
-    example 'multiple times' do
-      mocked_class.sing :kick, default: []
-      mock = mocked_class.new
-      mock.should have_been_told_to(:kick).times(0)
-      mock.kick
-      mock.should have_been_told_to(:kick).times(1)
-      mock.kick
-      mock.should have_been_told_to(:kick).times(2)
+
+    describe 'specifying number of times invoked' do
+      before { mocked_class.sing :wink, default: nil }
+      let(:instance) { mocked_class.new }
+
+      example 'default use case' do
+        instance.should have_been_told_to(:wink).times(0)
+        instance.wink
+        instance.should have_been_told_to(:wink).times(1)
+        instance.wink
+        instance.should have_been_told_to(:wink).times(2)
+      end
+
+      example 'failure message for should' do
+        expect { instance.should have_been_told_to(:wink).times(1) }.to \
+          raise_error(RSpec::Expectations::ExpectationNotMetError, /should have been told to wink 1 time but was told to wink 0 times/)
+
+        instance.wink
+        expect { instance.should have_been_told_to(:wink).times(2) }.to \
+          raise_error(RSpec::Expectations::ExpectationNotMetError, /should have been told to wink 2 times but was told to wink 1 time/)
+      end
+
+      example 'failure message for should not' do
+        expect { instance.should_not have_been_told_to(:wink).times(0) }.to \
+          raise_error(RSpec::Expectations::ExpectationNotMetError, /shouldn't have been told to wink 0 times, but was/)
+
+        instance.wink
+        expect { instance.should_not have_been_told_to(:wink).times(1) }.to \
+          raise_error(RSpec::Expectations::ExpectationNotMetError, /shouldn't have been told to wink 1 time, but was/)
+      end
     end
   end
 end
