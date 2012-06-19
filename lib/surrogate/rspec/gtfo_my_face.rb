a92d920ef46a1f40ffa7c90a1ea4b1ea7a86337f
Josh Cheek <josh.cheek@gmail.com>
2012-06-19 07:46:05 -0500

    Refactoring 63

--- lib/surrogate/rspec/gtfo_my_face.rb
+++ lib/surrogate/rspec/gtfo_my_face.rb
@@ -1,7 +1,6 @@
 class Surrogate
   module RSpec
     module MessagesFor
-
       MESSAGES = {
         verb: {
           should: {
@@ -61,6 +60,71 @@ class Surrogate
       end
 
       extend self
+    end
+
+
+
+    class TimesPredicate
+      attr_accessor :expected_times_invoked, :comparer
+      def initialize(expected_times_invoked=0, comparer=:<)
+        self.expected_times_invoked = expected_times_invoked
+        self.comparer = comparer
+      end
+
+      def matches?(invocations)
+        expected_times_invoked.send comparer, invocations.size
+      end
+
+      def default?
+        expected_times_invoked == 0 && comparer == :<
+      end
+    end
+
+    class WithFilter
+      attr_accessor :args, :block, :pass, :filter_name
+
+      def initialize(args=[], filter_name=:default_filter, &block)
+        self.args = args
+        self.block = block
+        self.pass = send filter_name
+        self.filter_name = filter_name
+      end
+
+      def filter(invocations)
+        invocations.select &pass
+      end
+
+      def default?
+        filter_name == :default_filter
+      end
+
+      private
+
+      def default_filter
+        Proc.new { true }
+      end
+
+      def args_must_match
+        lambda { |invocation| args_match? args, invocation }
+      end
+
+      def args_match?(expected_arguments, actual_arguments)
+        if expected_arguments.last.kind_of? Proc
+          return unless actual_arguments.last.kind_of? Proc
+          block_that_tests = expected_arguments.last
+          block_to_test = actual_arguments.last
+          asserter = Handler::BlockAsserter.new(block_to_test)
+          block_that_tests.call asserter
+          asserter.match?
+        else
+          if RSpec.rspec_mocks_loaded?
+            rspec_arg_expectation = ::RSpec::Mocks::ArgumentExpectation.new *expected_arguments
+            rspec_arg_expectation.args_match? *actual_arguments
+          else
+            expected_arguments == actual_arguments
+          end
+        end
+      end
     end
   end
 end
--- surrogate.gemspec
+++ surrogate.gemspec
@@ -18,7 +18,7 @@ Gem::Specification.new do |s|
   s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
   s.require_paths = ["lib"]
 
-  s.add_runtime_dependency 'bindable_block', '= 0.0.4'
+  s.add_runtime_dependency 'bindable_block', '= 0.0.5.1'
 
   s.add_development_dependency "rspec", '~> 2.8.0' # TODO: Figure out how far back we can work with
 end
