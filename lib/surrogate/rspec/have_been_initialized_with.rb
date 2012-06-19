97b721f840bfa4c85a0b4463f7cd379a725bb206
Josh Cheek <josh.cheek@gmail.com>
2012-06-19 07:46:07 -0500

    Refactoring 142

--- lib/surrogate/rspec/have_been_initialized_with.rb
+++ lib/surrogate/rspec/have_been_initialized_with.rb
@@ -1,26 +1,11 @@
+require 'surrogate/rspec/have_been_told_to'
+
 class Surrogate
   module RSpec
-    class HaveBeenInitializedWith
-      LANGUAGE_TYPE = :verb
-
+    class HaveBeenInitializedWith < HaveBeenToldTo
       def initialize(*initialization_args, &block)
-        @handler = Handler.new :initialize, self.class::LANGUAGE_TYPE
-        handler.with *initialization_args, &block
-      end
-
-      attr_reader :handler
-
-      def matches?(mocked_instance)
-        handler.instance = mocked_instance
-        handler.match?
-      end
-
-      def failure_message_for_should_not
-        handler.failure_message_for_should_not
-      end
-
-      def failure_message_for_should
-        handler.failure_message_for_should
+        super :initialize
+        with *initialization_args, &block
       end
     end
   end
