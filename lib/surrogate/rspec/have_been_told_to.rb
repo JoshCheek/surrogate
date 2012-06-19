bee917b374911b2ea8c2f5da6a408b1a6485e5e4
Josh Cheek <josh.cheek@gmail.com>
2012-06-19 07:46:07 -0500

    Refactoring 128

--- lib/surrogate/rspec/have_been_told_to.rb
+++ lib/surrogate/rspec/have_been_told_to.rb
@@ -4,56 +4,6 @@ class Surrogate
   module RSpec
     class HaveBeenToldTo < InvocationMatcher
 
-      class AbstractFailureMessage
-        attr_accessor :method_name, :invocations, :with_filter, :times_predicate
-
-        def initialize(method_name, invocations, with_filter, times_predicate)
-          self.method_name     = method_name
-          self.invocations     = invocations
-          self.with_filter     = with_filter
-          self.times_predicate = times_predicate
-        end
-
-        def get_message
-          raise "I should have been overridden"
-        end
-
-        def times_invoked
-          invocations.size
-        end
-
-        def inspect_arguments(arguments)
-          inspected_arguments = arguments.map { |argument| inspect_argument argument }
-          inspected_arguments << 'no args' if inspected_arguments.empty?
-          "`" << inspected_arguments.join(", ") << "'"
-        end
-
-        def inspect_argument(to_inspect)
-          if RSpec.rspec_mocks_loaded? && to_inspect.respond_to?(:description)
-            to_inspect.description
-          else
-            to_inspect.inspect
-          end
-        end
-
-        def expected_arguments
-          with_filter.args
-        end
-
-        def times_msg(n)
-          "#{n} time#{'s' unless n == 1}"
-        end
-
-        def expected_times_invoked
-          times_predicate.expected_times_invoked
-        end
-
-        def times_invoked_with_expected_args
-          invocations.size
-        end
-      end
-
-
       class FailureMessageShouldDefault < AbstractFailureMessage
         def get_message
           "was never told to #{ method_name }"
