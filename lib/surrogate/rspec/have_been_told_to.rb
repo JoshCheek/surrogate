6744d8af549cf59c7eaf6c81a08d0aaf0c006114
Josh Cheek <josh.cheek@gmail.com>
2012-06-19 07:46:06 -0500

    Refactoring 85

--- lib/surrogate/rspec/have_been_told_to.rb
+++ lib/surrogate/rspec/have_been_told_to.rb
@@ -30,15 +30,15 @@ class Surrogate
       MESSAGES = {
         should: {
           default:    Message.new { "was never told to #{ method_name }" },
-          with:       Message.new("should have been told to <%= method_name %> with <%= inspect_arguments expected_arguments %>, but <%= actual_invocation %>"),
-          times:      Message.new("should have been told to <%= method_name %> <%= times_msg expected_times_invoked %> but was told to <%= method_name %> <%= times_msg invocations.size %>"),
-          with_times: Message.new("should have been told to <%= method_name %> <%= times_msg expected_times_invoked %> with <%= inspect_arguments expected_arguments %>, but <%= actual_invocation %>"),
+          with:       Message.new { "should have been told to #{ method_name } with #{ inspect_arguments expected_arguments }, but #{ actual_invocation }" },
+          times:      Message.new { "should have been told to #{ method_name } #{ times_msg expected_times_invoked } but was told to #{ method_name } #{ times_msg invocations.size }" },
+          with_times: Message.new { "should have been told to #{ method_name } #{ times_msg expected_times_invoked } with #{ inspect_arguments expected_arguments }, but #{ actual_invocation }" },
           },
         should_not: {
-          default:    Message.new("shouldn't have been told to <%= method_name %>, but was told to <%= method_name %> <%= times_msg invocations.size %>"),
-          with:       Message.new("should not have been told to <%= method_name %> with <%= inspect_arguments expected_arguments %>, but <%= actual_invocation %>"),
-          times:      Message.new("shouldn't have been told to <%= method_name %> <%= times_msg expected_times_invoked %>, but was"),
-          with_times: Message.new("should not have been told to <%= method_name %> <%= times_msg expected_times_invoked %> with <%= inspect_arguments expected_arguments %>, but <%= actual_invocation %>"),
+          default:    Message.new { "shouldn't have been told to #{ method_name }, but was told to #{ method_name } #{ times_msg invocations.size }" },
+          with:       Message.new { "should not have been told to #{ method_name } with #{ inspect_arguments expected_arguments }, but #{ actual_invocation }" },
+          times:      Message.new { "shouldn't have been told to #{ method_name } #{ times_msg expected_times_invoked }, but was" },
+          with_times: Message.new { "should not have been told to #{ method_name } #{ times_msg expected_times_invoked } with #{ inspect_arguments expected_arguments }, but #{ actual_invocation }" },
         },
       }
     end
