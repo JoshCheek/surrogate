05769b2870d625e844a6d39efd53037b6743e4a7
Josh Cheek <josh.cheek@gmail.com>
2012-06-19 07:46:05 -0500

    Refactoring 50

--- lib/surrogate/rspec/have_been_told_to.rb
+++ lib/surrogate/rspec/have_been_told_to.rb
@@ -3,14 +3,17 @@ class Surrogate
     class HaveBeenToldTo
       class TimesPredicate
         attr_accessor :expected_times_invoked, :comparer
-
-        def initialize(expected_times_invoked, comparer)
+        def initialize(expected_times_invoked=0, comparer=:<)
           self.expected_times_invoked = expected_times_invoked
           self.comparer = comparer
         end
 
         def matches?(invocations)
           expected_times_invoked.send comparer, invocations.size
+        end
+
+        def default?
+          expected_times_invoked == 0 && comparer == :<
         end
       end
 
@@ -71,7 +74,7 @@ class Surrogate
       def initialize(expected)
         self.subject = expected
         self.message_type = :default
-        self.times_predicate = TimesPredicate.new(0, :<)
+        self.times_predicate = TimesPredicate.new
         self.with_filter = WithFilter.new
       end
 
