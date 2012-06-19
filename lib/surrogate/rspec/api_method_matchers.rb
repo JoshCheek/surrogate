ef741ca1a58cf578de6981d7210c0356a68290f7
Josh Cheek <josh.cheek@gmail.com>
2012-06-19 07:46:04 -0500

    Refactoring 18

--- lib/surrogate/rspec/api_method_matchers.rb
+++ lib/surrogate/rspec/api_method_matchers.rb
@@ -153,16 +153,17 @@ class Surrogate
     module MatchWithArguments
 
       def match?
+        block_asserter = lambda { |invocation|
+          return unless invocation.last.kind_of? Proc
+          block_that_tests = expected_arguments.last
+          block_to_test = invocation.last
+          asserter = Handler::BlockAsserter.new(block_to_test)
+          block_that_tests.call asserter
+          asserter.match?
+        }
+
         if expected_arguments.last.kind_of? Proc
-          invocations.select { |invocation|
-            lambda {
-              return unless invocation.last.kind_of? Proc
-              block_that_tests = expected_arguments.last
-              block_to_test = invocation.last
-              asserter = Handler::BlockAsserter.new(block_to_test)
-              block_that_tests.call asserter
-              asserter.match?
-            }.call }.any?
+          invocations.select { |invocation| block_asserter[invocation] }.any?
         else
           invocations.any? { |invocation| args_match? invocation }
         end
