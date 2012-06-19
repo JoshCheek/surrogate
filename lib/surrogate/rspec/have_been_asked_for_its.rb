dd22f4ba3ae082498069effcf230f7dcaa8994d2
Josh Cheek <josh.cheek@gmail.com>
2012-06-19 07:46:05 -0500

    Refactoring 40

--- lib/surrogate/rspec/have_been_asked_for_its.rb
+++ lib/surrogate/rspec/have_been_asked_for_its.rb
@@ -63,6 +63,10 @@ class Surrogate
       attr_reader :handler
       attr_accessor :times_predicate, :with_filter
 
+      attr_accessor :instance, :subject, :message_type
+      attr_accessor :expected_times_invoked, :expected_arguments
+
+      # keep
       def initialize(expected)
         self.subject = expected
         self.message_type = :default
@@ -70,70 +74,28 @@ class Surrogate
         self.with_filter = WithFilter.new
       end
 
+      # keep
       def matches?(mocked_instance)
         self.instance = mocked_instance
         times_predicate.matches? with_filter.filter invocations
       end
 
-      attr_accessor :instance, :subject, :message_type
-      attr_accessor :expected_times_invoked, :expected_arguments
-
-
-
-      def message_for(message_category, message_type)
-        message = MessagesFor::MESSAGES[:noun][message_category].fetch(message_type)
-        ERB.new(message).result(binding)
-      end
-
-      def inspect_arguments(arguments)
-        inspected_arguments = arguments.map { |argument| inspect_argument argument }
-        inspected_arguments << 'no args' if inspected_arguments.empty?
-        "`" << inspected_arguments.join(", ") << "'"
-      end
-
-      def inspect_argument(to_inspect)
-        if RSpec.rspec_mocks_loaded? && to_inspect.respond_to?(:description)
-          to_inspect.description
-        else
-          to_inspect.inspect
-        end
-      end
-
+      # keep
       def invocations
         instance.invocations(subject)
       end
-
-      def times_invoked
-        invocations.size
-      end
-
-      def times_invoked_with_expected_args
-        invocations.select { |invocation| args_match? invocation }.size
-      end
-
-      def actual_invocation
-        if message_type == :with
-          return message_for :other, :not_invoked if times_invoked.zero?
-          inspected_invocations = invocations.map { |invocation| inspect_arguments invocation }
-          "got #{inspected_invocations.join ', '}"
-        else
-          return message_for :other, :not_invoked if times_invoked.zero?
-          "#{message_for :other, :invoked_description} #{times_msg times_invoked_with_expected_args}"
-        end
-      end
-
-      def times_msg(n)
-        "#{n} time#{'s' unless n == 1}"
-      end
-
+      # keep
+      #
       def failure_message_for_should
         message_for :should, message_type
       end
 
+      # keep
       def failure_message_for_should_not
         message_for :should_not, message_type
       end
 
+      # keep (refactor me)
       def times(times_invoked)
         if message_type == :with
           @times_predicate = TimesPredicate.new(times_invoked, :==)
@@ -146,6 +108,7 @@ class Surrogate
         self
       end
 
+      # keep (refactor me)
       def with(*arguments, &expectation_block)
         if message_type == :times
           self.message_type = :with_times
@@ -159,6 +122,56 @@ class Surrogate
         self
       end
 
+
+      # === messages (can we get this out of here) ===
+
+      # messages
+      def message_for(message_category, message_type)
+        message = MessagesFor::MESSAGES[:noun][message_category].fetch(message_type)
+        ERB.new(message).result(binding)
+      end
+
+      # messages
+      def inspect_arguments(arguments)
+        inspected_arguments = arguments.map { |argument| inspect_argument argument }
+        inspected_arguments << 'no args' if inspected_arguments.empty?
+        "`" << inspected_arguments.join(", ") << "'"
+      end
+
+      # messages
+      def inspect_argument(to_inspect)
+        if RSpec.rspec_mocks_loaded? && to_inspect.respond_to?(:description)
+          to_inspect.description
+        else
+          to_inspect.inspect
+        end
+      end
+
+      # messages
+      def times_invoked
+        invocations.size
+      end
+
+      # messages
+      def actual_invocation
+        times_invoked = invocations.size
+        times_invoked_with_expected_args = invocations.select { |invocation| args_match? invocation }.size
+        if message_type == :with
+          return message_for :other, :not_invoked if times_invoked.zero?
+          inspected_invocations = invocations.map { |invocation| inspect_arguments invocation }
+          "got #{inspected_invocations.join ', '}"
+        else
+          return message_for :other, :not_invoked if times_invoked.zero?
+          "#{message_for :other, :invoked_description} #{times_msg times_invoked_with_expected_args}"
+        end
+      end
+
+      # messages
+      def times_msg(n)
+        "#{n} time#{'s' unless n == 1}"
+      end
+
+      # messages (can we delete this?)
       def args_match?(actual_arguments)
         if RSpec.rspec_mocks_loaded?
           rspec_arg_expectation = ::RSpec::Mocks::ArgumentExpectation.new *expected_arguments
