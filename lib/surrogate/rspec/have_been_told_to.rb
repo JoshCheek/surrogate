905c21c601f569a85244afc6ef3ac95582bd59c1
Josh Cheek <josh.cheek@gmail.com>
2012-06-19 07:46:05 -0500

    Refactoring 44

--- lib/surrogate/rspec/have_been_told_to.rb
+++ lib/surrogate/rspec/have_been_told_to.rb
@@ -1,35 +1,177 @@
 class Surrogate
   module RSpec
     class HaveBeenToldTo
-      LANGUAGE_TYPE = :verb
+      class TimesPredicate
+        attr_accessor :expected_times_invoked, :comparer
 
-      def times(number)
-        handler.times number
-        self
+        def initialize(expected_times_invoked, comparer)
+          self.expected_times_invoked = expected_times_invoked
+          self.comparer = comparer
+        end
+
+        def matches?(invocations)
+          expected_times_invoked.send comparer, invocations.size
+        end
       end
 
-      def with(*arguments, &block)
-        handler.with *arguments, &block
-        self
+      class WithFilter
+        attr_accessor :args, :block, :pass
+
+        def initialize(args=[], filter_name=:default_filter, &block)
+          self.args = args
+          self.block = block
+          self.pass = send filter_name
+          @filter_name = filter_name
+        end
+
+        def filter(invocations)
+          invocations.select &pass
+        end
+
+      private
+
+        def default_filter
+          Proc.new { true }
+        end
+
+        def args_must_match
+          lambda { |invocation| args_match? args, invocation }
+        end
+
+        def args_match?(expected_arguments, actual_arguments)
+          if expected_arguments.last.kind_of? Proc
+            block_asserter = lambda {
+              return unless actual_arguments.last.kind_of? Proc
+              block_that_tests = expected_arguments.last
+              block_to_test = actual_arguments.last
+              asserter = Handler::BlockAsserter.new(block_to_test)
+              block_that_tests.call asserter
+              asserter.match?
+            }
+            block_asserter.call
+          else
+            if RSpec.rspec_mocks_loaded?
+              rspec_arg_expectation = ::RSpec::Mocks::ArgumentExpectation.new *expected_arguments
+              rspec_arg_expectation.args_match? *actual_arguments
+            else
+              expected_arguments == actual_arguments
+            end
+          end
+        end
       end
 
       attr_reader :handler
+      attr_accessor :times_predicate, :with_filter
 
+      attr_accessor :instance, :subject, :message_type
+      attr_accessor :expected_times_invoked, :expected_arguments
+
+      # keep
       def initialize(expected)
-        @handler = Handler.new expected, self.class::LANGUAGE_TYPE
+        self.subject = expected
+        self.message_type = :default
+        self.times_predicate = TimesPredicate.new(0, :<)
+        self.with_filter = WithFilter.new
       end
 
+      # keep
       def matches?(mocked_instance)
-        handler.instance = mocked_instance
-        handler.match?
+        self.instance = mocked_instance
+        times_predicate.matches? with_filter.filter invocations
       end
 
-      def failure_message_for_should_not
-        handler.failure_message_for_should_not
+      # keep
+      def invocations
+        instance.invocations(subject)
       end
 
+      # keep
       def failure_message_for_should
-        handler.failure_message_for_should
+        message_for :should, message_type
+      end
+
+      # keep
+      def failure_message_for_should_not
+        message_for :should_not, message_type
+      end
+
+      # keep (refactor me)
+      def times(times_invoked)
+        @times_predicate = TimesPredicate.new(times_invoked, :==)
+
+        if message_type == :with
+          self.message_type = :with_times
+        else
+          self.message_type = :times
+        end
+        self.expected_times_invoked = times_invoked
+        self
+      end
+
+      # keep (refactor me)
+      def with(*arguments, &expectation_block)
+        self.with_filter = WithFilter.new arguments, :args_must_match,  &expectation_block
+
+        if message_type == :times
+          self.message_type = :with_times
+        else
+          self.message_type = :with
+        end
+        arguments << expectation_block if expectation_block
+        self.expected_arguments = arguments
+        self
+      end
+
+
+      # === messages (can we get this out of here) ===
+
+      def message_for(message_category, message_type)
+        message = MessagesFor::MESSAGES[:verb][message_category].fetch(message_type)
+        ERB.new(message).result(binding)
+      end
+
+      def inspect_arguments(arguments)
+        inspected_arguments = arguments.map { |argument| inspect_argument argument }
+        inspected_arguments << 'no args' if inspected_arguments.empty?
+        "`" << inspected_arguments.join(", ") << "'"
+      end
+
+      def inspect_argument(to_inspect)
+        if RSpec.rspec_mocks_loaded? && to_inspect.respond_to?(:description)
+          to_inspect.description
+        else
+          to_inspect.inspect
+        end
+      end
+
+      def times_invoked
+        invocations.size
+      end
+
+      def actual_invocation
+        times_invoked = invocations.size
+        times_invoked_with_expected_args = invocations.select { |invocation| args_match? invocation }.size
+        if message_type == :with
+          return message_for :other, :not_invoked if times_invoked.zero?
+          inspected_invocations = invocations.map { |invocation| inspect_arguments invocation }
+          "got #{inspected_invocations.join ', '}"
+        else
+          return message_for :other, :not_invoked if times_invoked.zero?
+          "#{message_for :other, :invoked_description} #{times_msg times_invoked_with_expected_args}"
+        end
+      end
+
+      def times_msg(n)
+        "#{n} time#{'s' unless n == 1}"
+      end
+
+      def args_match?(actual_arguments)
+        if RSpec.rspec_mocks_loaded?
+          rspec_arg_expectation = ::RSpec::Mocks::ArgumentExpectation.new *expected_arguments
+          rspec_arg_expectation.args_match? *actual_arguments
+        else
+          expected_arguments == actual_arguments
+        end
       end
     end
   end
