029b2b5322834cfffe6a9f67a72de5eb8c75312a
Josh Cheek <josh.cheek@gmail.com>
2012-06-19 07:46:04 -0500

    Refactoring 21

--- lib/surrogate/rspec/have_been_told_to.rb
+++ lib/surrogate/rspec/have_been_told_to.rb
@@ -3,8 +3,6 @@ class Surrogate
     class HaveBeenToldTo
       LANGUAGE_TYPE = :verb
 
-      include CommonMatcher
-
       def times(number)
         handler.times number
         self
@@ -13,6 +11,25 @@ class Surrogate
       def with(*arguments, &block)
         handler.with *arguments, &block
         self
+      end
+
+      attr_reader :handler
+
+      def initialize(expected)
+        @handler = Handler.new expected, self.class::LANGUAGE_TYPE
+      end
+
+      def matches?(mocked_instance)
+        handler.instance = mocked_instance
+        handler.match?
+      end
+
+      def failure_message_for_should_not
+        handler.failure_message_for_should_not
+      end
+
+      def failure_message_for_should
+        handler.failure_message_for_should
       end
     end
   end
