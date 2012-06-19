e8dd01340b3a6039ac7dd49ef43c2e5931cc0752
Josh Cheek <josh.cheek@gmail.com>
2012-06-19 07:46:06 -0500

    Refactoring 90

--- lib/surrogate/rspec/have_been_told_to.rb
+++ lib/surrogate/rspec/have_been_told_to.rb
@@ -3,7 +3,7 @@ require 'surrogate/rspec/invocation_matcher'
 class Surrogate
   module RSpec
     class HaveBeenToldTo < InvocationMatcher
-      class FailureMessage
+      class FailureMessageBlock
         def initialize(&message)
           @message = message
         end
@@ -13,18 +13,39 @@ class Surrogate
         end
       end
 
+      class FailureMessageInternal
+        def result(env)
+          @env = env
+          get_message
+        end
+
+        def get_message
+          raise "I should have been overridden"
+        end
+
+        def method_name
+          @env.method_name
+        end
+      end
+
+      class FailureMessageShouldDefault < FailureMessageInternal
+        def get_message
+          "was never told to #{ method_name }"
+        end
+      end
+
       MESSAGES = {
         should: {
-          default:    FailureMessage.new { "was never told to #{ method_name }" },
-          with:       FailureMessage.new { "should have been told to #{ method_name } with #{ inspect_arguments expected_arguments }, but #{ actual_invocation }" },
-          times:      FailureMessage.new { "should have been told to #{ method_name } #{ times_msg expected_times_invoked } but was told to #{ method_name } #{ times_msg invocations.size }" },
-          with_times: FailureMessage.new { "should have been told to #{ method_name } #{ times_msg expected_times_invoked } with #{ inspect_arguments expected_arguments }, but #{ actual_invocation }" },
+          default:    FailureMessageShouldDefault.new,
+          with:       FailureMessageBlock.new { "should have been told to #{ method_name } with #{ inspect_arguments expected_arguments }, but #{ actual_invocation }" },
+          times:      FailureMessageBlock.new { "should have been told to #{ method_name } #{ times_msg expected_times_invoked } but was told to #{ method_name } #{ times_msg invocations.size }" },
+          with_times: FailureMessageBlock.new { "should have been told to #{ method_name } #{ times_msg expected_times_invoked } with #{ inspect_arguments expected_arguments }, but #{ actual_invocation }" },
           },
         should_not: {
-          default:    FailureMessage.new { "shouldn't have been told to #{ method_name }, but was told to #{ method_name } #{ times_msg invocations.size }" },
-          with:       FailureMessage.new { "should not have been told to #{ method_name } with #{ inspect_arguments expected_arguments }, but #{ actual_invocation }" },
-          times:      FailureMessage.new { "shouldn't have been told to #{ method_name } #{ times_msg expected_times_invoked }, but was" },
-          with_times: FailureMessage.new { "should not have been told to #{ method_name } #{ times_msg expected_times_invoked } with #{ inspect_arguments expected_arguments }, but #{ actual_invocation }" },
+          default:    FailureMessageBlock.new { "shouldn't have been told to #{ method_name }, but was told to #{ method_name } #{ times_msg invocations.size }" },
+          with:       FailureMessageBlock.new { "should not have been told to #{ method_name } with #{ inspect_arguments expected_arguments }, but #{ actual_invocation }" },
+          times:      FailureMessageBlock.new { "shouldn't have been told to #{ method_name } #{ times_msg expected_times_invoked }, but was" },
+          with_times: FailureMessageBlock.new { "should not have been told to #{ method_name } #{ times_msg expected_times_invoked } with #{ inspect_arguments expected_arguments }, but #{ actual_invocation }" },
         },
       }
     end
