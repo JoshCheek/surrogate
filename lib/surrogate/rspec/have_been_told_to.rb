e5ea65d3fb90202feb7f70993777c75dbfb39d17
Josh Cheek <josh.cheek@gmail.com>
2012-06-19 07:46:06 -0500

    Refactoring 70

--- lib/surrogate/rspec/have_been_told_to.rb
+++ lib/surrogate/rspec/have_been_told_to.rb
@@ -1,6 +1,8 @@
+require 'surrogate/rspec/invocation_matcher'
+
 class Surrogate
   module RSpec
-    class HaveBeenToldTo
+    class HaveBeenToldTo < InvocationMatcher
       MESSAGES = {
         should: {
           default:    "was never told to <%= method_name %>",
@@ -15,51 +17,6 @@ class Surrogate
           with_times: "should not have been told to <%= method_name %> <%= times_msg expected_times_invoked %> with <%= inspect_arguments expected_arguments %>, but <%= actual_invocation %>",
         },
       }
-
-      attr_accessor :times_predicate, :with_filter
-      attr_accessor :instance, :method_name
-
-      def initialize(method_name)
-        self.method_name = method_name
-        self.times_predicate = TimesPredicate.new
-        self.with_filter = WithFilter.new
-      end
-
-      def matches?(mocked_instance)
-        self.instance = mocked_instance
-        times_predicate.matches? filtered_args
-      end
-
-      def filtered_args
-        @filtered_args ||= with_filter.filter invocations
-      end
-
-      def invocations
-        instance.invocations(method_name)
-      end
-
-      def failure_message_for_should
-        message_for :should
-      end
-
-      def failure_message_for_should_not
-        message_for :should_not
-      end
-
-      def times(times_invoked)
-        @times_predicate = TimesPredicate.new(times_invoked, :==)
-        self
-      end
-
-      def with(*arguments, &expectation_block)
-        self.with_filter = WithFilter.new arguments, :args_must_match,  &expectation_block
-        arguments << expectation_block if expectation_block
-        self
-      end
-
-      def message_for(message_category)
-        FailureMessages.new.messages(message_category, with_filter, times_predicate, method_name, invocations, MESSAGES)
-      end
     end
   end
 end
---
+++ lib/surrogate/rspec/invocation_matcher.rb
@@ -0,0 +1,50 @@
+class Surrogate
+  module RSpec
+    class InvocationMatcher
+      attr_accessor :times_predicate, :with_filter
+      attr_accessor :instance, :method_name
+
+      def initialize(method_name)
+        self.method_name = method_name
+        self.times_predicate = TimesPredicate.new
+        self.with_filter = WithFilter.new
+      end
+
+      def matches?(mocked_instance)
+        self.instance = mocked_instance
+        times_predicate.matches? filtered_args
+      end
+
+      def filtered_args
+        @filtered_args ||= with_filter.filter invocations
+      end
+
+      def invocations
+        instance.invocations(method_name)
+      end
+
+      def failure_message_for_should
+        message_for :should
+      end
+
+      def failure_message_for_should_not
+        message_for :should_not
+      end
+
+      def times(times_invoked)
+        @times_predicate = TimesPredicate.new(times_invoked, :==)
+        self
+      end
+
+      def with(*arguments, &expectation_block)
+        self.with_filter = WithFilter.new arguments, :args_must_match,  &expectation_block
+        arguments << expectation_block if expectation_block
+        self
+      end
+
+      def message_for(message_category)
+        FailureMessages.new.messages(message_category, with_filter, times_predicate, method_name, invocations, self.class::MESSAGES)
+      end
+    end
+  end
+end
