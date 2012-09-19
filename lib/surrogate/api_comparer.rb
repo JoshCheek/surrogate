3ddf5a19be4576f9022bb4ebdcaaff4022892de1
Josh Cheek <josh.cheek@gmail.com>
2012-09-19 01:39:39 -0500

    Extract method reflection from api comparer

--- lib/surrogate/api_comparer.rb
+++ lib/surrogate/api_comparer.rb
@@ -1,4 +1,6 @@
 require 'set'
+require 'surrogate/api_comparer/surrogate_methods'
+require 'surrogate/api_comparer/actual_methods'
 
 class Surrogate
 
@@ -101,97 +103,6 @@ class Surrogate
       obj = Object.new
       obj.singleton_class.send :define_method, :abc123, &proc
       obj.method :abc123
-    end
-
-
-    # methods from the actual class (as opposed to "these are actually methods"
-    class ActualMethods < Struct.new(:actual)
-      def methods
-        { instance: {
-            inherited: instance_inherited_methods,
-            other:     instance_other_methods,
-          },
-          class: {
-            inherited: class_inherited_methods,
-            other:     class_other_methods,
-          },
-        }
-      end
-
-      def instance_inherited_methods
-        Set.new actual.instance_methods - actual.instance_methods(false)
-      end
-
-      def instance_other_methods
-        Set.new(actual.instance_methods) - instance_inherited_methods
-      end
-
-      def class_inherited_methods
-        Set.new actual.singleton_class.instance_methods - actual.singleton_class.instance_methods(false)
-      end
-
-      def class_other_methods
-        Set.new(actual.singleton_class.instance_methods) - class_inherited_methods
-      end
-    end
-
-
-    class SurrogateMethods < Struct.new(:surrogate)
-      def methods
-        { instance: {
-            api:            instance_api_methods,
-            inherited:      instance_inherited_methods,
-            other:          instance_other_methods,
-            without_bodies: instance_without_bodies,
-          },
-          class: {
-            api:            class_api_methods,
-            inherited:      class_inherited_methods,
-            other:          class_other_methods,
-            without_bodies: class_without_bodies,
-          },
-        }
-      end
-
-      def instance_api_methods
-        Set.new surrogate.api_method_names
-      end
-
-      def instance_inherited_methods
-        Set.new surrogate.instance_methods - surrogate.instance_methods(false)
-      end
-
-      def instance_other_methods
-        Set.new(surrogate.instance_methods false) - instance_api_methods
-      end
-
-      def instance_without_bodies
-        Set.new class_hatchery.api_method_names.reject { |name| class_hatchery.api_method_for name }
-      end
-
-      def class_api_methods
-        Set.new surrogate.singleton_class.api_method_names
-      end
-
-      def class_inherited_methods
-        Set.new surrogate.singleton_class.instance_methods - surrogate.singleton_class.instance_methods(false)
-      end
-
-      def class_other_methods
-        Set.new(surrogate.singleton_class.instance_methods false) - class_api_methods - class_inherited_methods
-      end
-
-      def class_without_bodies
-        Set.new singleton_class_hatchery.api_method_names.reject { |name| singleton_class_hatchery.api_method_for name }
-      end
-
-      def class_hatchery
-        @class_hatchery ||= surrogate.instance_variable_get :@hatchery
-      end
-
-      def singleton_class_hatchery
-        @singleton_class_hatchery ||= surrogate.singleton_class.instance_variable_get :@hatchery
-      end
     end
   end
 end
---
+++ lib/surrogate/api_comparer/surrogate_methods.rb
@@ -0,0 +1,64 @@
+require 'set'
+class Surrogate
+  class ApiComparer
+
+    # reflects on the surrogate to give info about methods that are useful for the comparer
+    class SurrogateMethods < Struct.new(:surrogate)
+      def methods
+        { instance: {
+            api:            instance_api_methods,
+            inherited:      instance_inherited_methods,
+            other:          instance_other_methods,
+            without_bodies: instance_without_bodies,
+          },
+          class: {
+            api:            class_api_methods,
+            inherited:      class_inherited_methods,
+            other:          class_other_methods,
+            without_bodies: class_without_bodies,
+          },
+        }
+      end
+
+      def instance_api_methods
+        Set.new surrogate.api_method_names
+      end
+
+      def instance_inherited_methods
+        Set.new surrogate.instance_methods - surrogate.instance_methods(false)
+      end
+
+      def instance_other_methods
+        Set.new(surrogate.instance_methods false) - instance_api_methods
+      end
+
+      def instance_without_bodies
+        Set.new class_hatchery.api_method_names.reject { |name| class_hatchery.api_method_for name }
+      end
+
+      def class_api_methods
+        Set.new surrogate.singleton_class.api_method_names
+      end
+
+      def class_inherited_methods
+        Set.new surrogate.singleton_class.instance_methods - surrogate.singleton_class.instance_methods(false)
+      end
+
+      def class_other_methods
+        Set.new(surrogate.singleton_class.instance_methods false) - class_api_methods - class_inherited_methods
+      end
+
+      def class_without_bodies
+        Set.new singleton_class_hatchery.api_method_names.reject { |name| singleton_class_hatchery.api_method_for name }
+      end
+
+      def class_hatchery
+        @class_hatchery ||= surrogate.instance_variable_get :@hatchery
+      end
+
+      def singleton_class_hatchery
+        @singleton_class_hatchery ||= surrogate.singleton_class.instance_variable_get :@hatchery
+      end
+    end
+  end
+end
